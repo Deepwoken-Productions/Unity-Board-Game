a6f87e0545fd18dbde5e881aba798aa5063d0d1c
TypeRed <91504929+TypeRed@users.noreply.github.com>
2021-10-09 12:30:44 -0400

    Cards Script and Sprite

---
+++ Assets/Scripts/Cards/Deck.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+
+    [SerializeField]
+    //Creates a list for the cards which will be used to get the cards
+    public List<Card> deck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //A method to get the Scriptable Object into the card
+    public void getCard() 
+    {
+        Card newCard = deck[rndCard()];
+        cardSwap(newCard);
+    }
+
+    //A method to generate and return a random number form 0 to 4, used for randomizing cards in the list
+    public int rndCard()
+    {
+        float rndNum = Random.Range(0, 4);
+
+        return (int)rndNum;
+    }
+
+    //Method to swap the Scriptable Object thats created for the card
+    public void cardSwap(Card newCard) 
+    {
+        Card currentCard = GameObject.Find("Canvas").transform.GetChild(0).gameObject.GetComponent<cardDisplay>().card;
+        currentCard = newCard;
+    }
+}
